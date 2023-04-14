47e7c79051078112f2842e860e82f7d9f8013223
abdourahman <aj22018057@utg.edu.gm>
2023-04-14 22:55:46 +0000

    update

--- src/weather/SearchAddress.jsx
+++ src/weather/SearchAddress.jsx
@@ -1,81 +1,67 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react'
-import Spinner from '../components/layout/Spinner';
-import WeatherItem from './WeatherItem';
+import axios from "axios";
+import { useState } from "react";
+import Spinner from "../components/layout/Spinner";
+import WeatherItem from "./WeatherItem";
 
 function SearchAddress() {
-    const [address, setAddress] = useState('')
+    const [address, setAddress] = useState("");
     const [coordinates, setCoordinates] = useState(null);
-    const [weather, setWeather] = useState(null)
-    const [loading, setLoading] = useState(false)
-    
-    useEffect(() => {
-            const fetchCoordinates = async (location) => {
-                setLoading(true)
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-                try {
-                    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${process.env.REACT_APP_OPENWEATHER_API}`);
-                
-                    const resp = await response.json();
-                    setCoordinates(resp)
-                    const latitude = coordinates[0].lat;      
-                    const longitude = coordinates[0].lon;  
+    const fetchCoordinates = async (e) => {
+        if (e.key === "Enter") {
+            setLoading(true);
 
-                    const weatherRes = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API}&units=metric`)
-                    const res = weatherRes.data;
-                    setWeather(res)
-                } catch (error) {
-                    console.log(error.message)
-                } finally {
-                    setLoading(false)
-                }
+            try {
+                const response = await axios.get(
+                    `https://api.openweathermap.org/geo/1.0/direct?q=${address}&limit=1&appid=${process.env.REACT_APP_OPENWEATHER_API}`
+                );
+
+                const resp = await response.data;
+                setCoordinates(resp);
+                const latitude = resp[0].lat;
+                const longitude = resp[0].lon;
+                console.log(resp);
+                setAddress("");
+
+                const weatherRes = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API}&units=metric`
+                );
+                const res = weatherRes.data;
+                setWeather(res);
+                console.log(res);
+                setLoading(false);
+            } catch (error) {
+                console.log(error.message);
+            } finally {
+                setLoading(false);
             }
-
-        fetchCoordinates(address)
-        // eslint-disable-next-line
-    }, [address, weather])
-
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        if (address === '') {
-            alert('Please enter something...')
-        } else {
-                setAddress('');
-            }
-    }
-    
-    const onChange = (e) => {
-        setAddress(e.target.value)
-    }
+        }
+    };
 
     return (
-        <div className="grid grid-cols-1 mb-12">
+        <div className="grid mb-12 place-content-center place-items-center min-w-fit">
             <div className="form-control mb-12">
-                <form onSubmit={onSubmit}>
-                    <div className="relative">
-                        <input
-                            type="text"
-                            placeholder='City, Place...'
-                            className='input input-lg bg-white text-black w-full'
-                            value={address}
-                            id='address'
-                            onChange={onChange}
-                        />
-                        <button type='submit' className='absolute top-0 right-0 btn btn-lg rounded-l-none text-gray-500'>Search</button>
-                    </div>
-                </form>
+                <div className="relative">
+                    <input
+                        type="text"
+                        placeholder="City, Place..."
+                        className="input input-md bg-white text-black"
+                        value={address}
+                        id="address"
+                        onChange={(e) => setAddress(e.target.value)}
+                        onKeyUp={fetchCoordinates}
+                    />
+                </div>
             </div>
-        
-            {loading ? <Spinner /> :
-                <>
-                    { weather &&
-                        <WeatherItem weather={weather} />
-                    }
-                </>
-            }
+            {loading ? (
+                <Spinner />
+            ) : (
+                <>{weather && <WeatherItem weather={weather} />}</>
+            )}
         </div>
-    )
+    );
 }
 
-export default SearchAddress
+export default SearchAddress;
